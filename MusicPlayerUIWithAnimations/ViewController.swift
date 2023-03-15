1263d1673260ab77a358e1652ec97b6c96dd230a
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-03-15 20:38:16 +0300

    musicPlayer ui and animations are done

--- MusicPlayerUIWithAnimations/ViewController.swift
+++ MusicPlayerUIWithAnimations/ViewController.swift
@@ -42,18 +42,61 @@ class ViewController: UIViewController {
     private func setupUIButtons() {
         reverseBackgroundView.layer.cornerRadius = 35
         reverseBackgroundView.clipsToBounds = true
-        reverseBackgroundView.alpha = 0.2
+        reverseBackgroundView.alpha = 0
         
         playPauseBackgroundView.layer.cornerRadius = 35
         playPauseBackgroundView.clipsToBounds = true
-        playPauseBackgroundView.alpha = 0.2
+        playPauseBackgroundView.alpha = 0
         
         forwardBackgroundView.layer.cornerRadius = 35
         forwardBackgroundView.clipsToBounds = true
-        forwardBackgroundView.alpha = 0.2
+        forwardBackgroundView.alpha = 0
     }
     
     //MARK: ACTIONS
+    
+    @IBAction func touchDown(_ button: UIButton) {
+        print("Touch Down button tapped")
+        let buttonBackgroundView: UIView
+        switch button {
+        case reverseButton:
+            buttonBackgroundView = reverseBackgroundView
+        case forwardButton:
+            buttonBackgroundView = forwardBackgroundView
+        case playPauseButton:
+            buttonBackgroundView = playPauseBackgroundView
+        default:
+            return
+        }
+        UIView.animate(withDuration: 0.25, delay: 0) {
+            buttonBackgroundView.alpha = 0.5
+            button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }
+    }
+    
+    @IBAction func touchUpInside(_ button: UIButton) {
+        print("Touch Up Inside button tapped")
+        let buttonBackgroundView: UIView
+        switch button {
+        case reverseButton:
+            buttonBackgroundView = reverseBackgroundView
+        case forwardButton:
+            buttonBackgroundView = forwardBackgroundView
+        case playPauseButton:
+            buttonBackgroundView = playPauseBackgroundView
+        default:
+            return
+        }
+        UIView.animate(withDuration: 0.25, delay: 0) {
+            buttonBackgroundView.alpha = 0
+            buttonBackgroundView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            button.transform = .identity
+        } completion: { _ in
+            buttonBackgroundView.transform = .identity
+        }
+        
+    }
+    
     @IBAction func playPauseButtonTapped (_ button: UIButton) {
         if isPlaying {
             UIView.animate(withDuration: 0.5) {
